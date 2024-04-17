438f68ee7c86d27851840413e09c8f903a252505
stvchua <stv.chua@gmail.com>
2024-04-17 10:30:59 +0800

    Initial commit

---
+++ report_files/analysis.R
@@ -0,0 +1,88 @@
+data <- read.csv("https://raw.githubusercontent.com/holtzy/R-graph-gallery/master/DATA/data_2.csv")
+
+summary(data)
+
+print(round(mean(subset(na.omit(data), species == "Adelie" & island == "Torgersen")$bill_length_mm), 2))
+print(round(mean(subset(na.omit(data), species == "Adelie" & island == "Biscoe")$bill_length_mm), 2))
+print(round(mean(subset(na.omit(data), species == "Adelie" & island == "Dream")$bill_length_mm), 2))
+
+
+# Plot
+penguins_clean <- na.omit(data)
+plot(penguins_clean$bill_length_mm, penguins_clean$bill_depth_mm, type = "n", xlab = "Bill Length (mm)", ylab = "Bill Depth (mm)", main = "Penguin Bill Dimensions")
+points(
+  penguins_clean$bill_length_mm[penguins_clean$species == "Adelie"], penguins_clean$bill_depth_mm[penguins_clean$species == "Adelie"],
+  col = "red", pch = 16
+)
+points(penguins_clean$bill_length_mm[penguins_clean$species == "Chinstrap"], penguins_clean$bill_depth_mm[penguins_clean$species == "Chinstrap"], col = "green", pch = 17)
+points(penguins_clean$bill_length_mm[penguins_clean$species == "Gentoo"],
+  penguins_clean$bill_depth_mm[penguins_clean$species == "Gentoo"],
+  col = "blue", pch = 18
+)
+legend("topright",
+  legend = unique(penguins_clean$species),
+  col = c(
+    "red",
+    "green",
+    "blue"
+  ), pch = c(16, 17, 18)
+)
+
+## Module 1, Lesson 2 ----
+
+library(tidyverse)
+
+# setwd("~/perso/tmp/project/penguins")
+
+# Read data using readr
+data <- readr::read_csv("https://raw.githubusercontent.com/holtzy/R-graph-gallery/master/DATA/data_2.csv")
+
+# Summary
+summary(data)
+
+# Calculating mean bill length for different species and islands using dplyr
+data %>%
+  filter(species == "Adelie") %>%
+  group_by(island) %>%
+  summarize(mean_bill_length = round(mean(bill_length_mm, na.rm = TRUE), 2))
+
+# Plot using ggplot2
+data %>%
+  na.omit() %>%
+  ggplot(aes(x = bill_length_mm, y = bill_depth_mm, color = species, shape = species)) +
+  geom_point() +
+  labs(x = "Bill Length (mm)", y = "Bill Depth (mm)", title = "Penguin Bill Dimensions") +
+  scale_shape_manual(values = c("Adelie" = 16, "Chinstrap" = 17, "Gentoo" = 18))
+
+library(readxl)
+data <- read_excel("input/data.xlsx")
+
+## Module 1, Lesson 8 ----
+# plot function
+create_scatterplot <- function(data, selected_species, selected_island) {
+  # Filter the data for the specified species and island
+  filtered_data <- data %>%
+    na.omit() %>%
+    filter(species == selected_species, island == selected_island)
+  
+  # Create the scatterplot
+  plot <- ggplot(
+    filtered_data,
+    aes(x = bill_length_mm, y = bill_depth_mm, color = species, shape = species)
+  ) +
+    geom_point() +
+    labs(
+      x = "Bill Length (mm)",
+      y = "Bill Depth (mm)",
+      title = paste("Penguin Bill Dimensions -", selected_species, "-", selected_island)
+    )
+  
+  return(plot)
+}
+
+source(file = "functions.R")
+# Use the function
+create_scatterplot(data, "Adelie", "Torgersen")
+
+## Module 2, Lesson 2 ----
+library(rmarkdown)
